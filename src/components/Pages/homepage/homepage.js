a00578062b15d1b514547dfa449649afa8c952d7
ashivs2500 <antonioshivers2500@yahoo.com>
2021-03-25 14:37:14 -0400

    removed videos&images folder

--- src/components/Pages/homepage/homepage.js
+++ src/components/Pages/homepage/homepage.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './homepage.scss'
+import inuyashaImg from '../Inuyasha/inuyashaCover.jpg'
 
 const homepage = () => (
   <div className='container-home'>
     <div className='homeBox'>
-      <Link to='/Inuyasha' className='inuyasha-img'>lol</Link>
+      <Link to='/Inuyasha'>
+        <img width="400" height="400" src={inuyashaImg} className="inuyasha-img"></img>
+      </Link>
     </div>
   </div>
 )
