5a283939fb6992c1b7f1054ce562dc7994b0347e
ashivs2500 <antonioshivers2500@yahoo.com>
2021-03-25 12:48:36 -0400

    added inuyasha first 10 eps

--- src/components/Pages/homepage/homepage.js
+++ src/components/Pages/homepage/homepage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './homepage.scss'
 
 const homepage = () => (
   <div className='container-home'>
     <div className='homeBox'>
-      lol
+      <Link to='/Inuyasha' className='inuyasha-img'>lol</Link>
     </div>
   </div>
 )
