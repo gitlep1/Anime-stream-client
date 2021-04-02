8341b0620daba957c4e64db83bbdf338e71e57ac
ashivs2500 <antonioshivers2500@yahoo.com>
2021-04-02 13:50:53 -0400

    updated app.js with the homepages of dbz and hxh

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import animeList from './components/Pages/AnimeList/animelist'
 import inuyasha from './components/Pages/Inuyasha/inuyasha'
 import bleach from './components/Pages/Bleach/bleach'
 import deathnote from './components/Pages/Deathnote/deathnote'
-// import dbz from './components/Pages/Dragonballz/dbz'
-// import hxh from './components/Pages/Hunterxhunter/hxh'
+import dbz from './components/Pages/Dragonballz/dbz'
+import hxh from './components/Pages/Hunterxhunter/hxh'
 // import naruto from './components/Pages/Naruto/naruto'
 // import onepiece from './components/Pages/Onepiece/op'
 // import yyh from './components/Pages/Yuyuhakusho/yyh'
@@ -83,9 +83,9 @@ class App extends Component {
           <Route path='/inuyasha/' component={inuyasha} />
           <Route path='/Bleach/' component={bleach} />
           <Route path='/Deathnote/' component={deathnote} />
-          {/* <Route path='/Dragonballz/' component={dbz} />
+          <Route path='/Dragonballz/' component={dbz} />
           <Route path='/Hunterxhunter/' component={hxh} />
-          <Route path='/Naruto/' component={naruto} />
+          {/* <Route path='/Naruto/' component={naruto} />
           <Route path='/Onepiece/' component={onepiece} />
           <Route path='/Yuyuhakusho/' component={yyh} /> */}
         </main>
